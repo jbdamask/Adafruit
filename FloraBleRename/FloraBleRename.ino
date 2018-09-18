50a8e654540f798bc5b6da74ccbd0b2bb3b96d9b
johndamask <jbdamask@gmail.com>
2018-09-18 06:44:32 -0400

    Simple rename of BLE board

--- FloraBleRename/FloraBleRename.ino
+++ FloraBleRename/FloraBleRename.ino
@@ -1,11 +1,14 @@
-// Basic sketch showing how to change the advertised name of an Adafruit Bluefruit LE board
+/* 
+  Basic sketch showing how to change the advertised name of an Adafruit Bluefruit LE board
+  Check with any BLE sniffer like Adafruit Bluefruit LE Connect app 
+*/
 #include "Adafruit_BluefruitLE_UART.h"
 #define BLE_NAME "AT+GAPDEVNAME=I changed the name"
 Adafruit_BluefruitLE_UART ble(Serial1, -1);
 
 void setup() {
   ble.begin(true);   // Start it up  
-  ble.println(BLE_NAME); // Change advertised name 
+  ble.println(BLE_NAME); // Change advertised name
 }
 
 void loop() {
